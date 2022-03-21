a6c8dd69729f0aac03e69c9adfbc4ea61616862d
Amer Ibric <ameribric@gmail.com>
2022-03-21 15:11:12 -0400

    commit tree 195d1cf1a684a14b2938980b3a8dfc43e4cda947

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function saturdayFun(statement = "This Saturday, ", skate = "roller-skate") {
+console.log(`${statement} I will ${skate}`);
+}
+saturdayFun(statement, skate)
+
+
+function mondayWork () {
+    return 'This Monday, I will go to the office'
+}
+mondayWork()
